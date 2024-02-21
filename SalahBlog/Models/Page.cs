e629167f9475eedabec1fc28b7d01f902f375cc5
Mohamed salah <Mohamed salah@DESKTOP-9K3BDNR>
2024-02-21 16:49:14 +0200

    Add project files.

---
+++ SalahBlog/Models/Page.cs
@@ -0,0 +1,11 @@
+﻿namespace SalahBlog.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Descriotion { get; set; }
+        public string? Slug { get; set; }
+    }
+}
