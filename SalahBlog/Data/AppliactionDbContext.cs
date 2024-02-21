baa9c283078cd7225976598f723ca6724d461a42
Mohamed salah <Mohamed salah@DESKTOP-9K3BDNR>
2024-02-21 17:06:48 +0200

    Add Db Context

---
+++ SalahBlog/Data/AppliactionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SalahBlog.Models;
+
+namespace SalahBlog.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+        public DbSet<ApplicationUser>? ApplicationUsers { get; set; }
+        public DbSet<Post>? Post { get; set; }
+        public DbSet<Page>? Page { get; set; }
+    }
+}
--- SalahBlog/Program.cs
+++ SalahBlog/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SalahBlog.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
