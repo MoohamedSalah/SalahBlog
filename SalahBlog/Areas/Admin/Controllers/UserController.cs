4f5484d288dc60f0fbb96505a0485353e1b83db7
Mohamed salah <Mohamed salah@DESKTOP-9K3BDNR>
2024-02-25 14:21:53 +0200

    Login Function added

--- SalahBlog/Areas/Admin/Controllers/UserController.cs
+++ SalahBlog/Areas/Admin/Controllers/UserController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalahBlog.Models;
+using SalahBlog.ViewModels;
 
 namespace SalahBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly INotyfService _notyfService;
+        public UserController(UserManager<ApplicationUser> userManager,
+                              SignInManager<ApplicationUser> signInManager,
+                              INotyfService notyfService) 
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _notyfService = notyfService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,7 +29,27 @@ namespace SalahBlog.Areas.Admin.Controllers
         [HttpGet("Login")]
         public IActionResult Login()
         {
-            return View();
+            return View(new LoginVM());
+        }  
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if(!ModelState.IsValid) { return View(loginVM); }
+            var userExist=await _userManager.Users.FirstOrDefaultAsync(x=>x.UserName==loginVM.Username);
+            if (userExist == null) 
+            {
+                _notyfService.Error("Username does not exist");
+                return View(loginVM); 
+            }
+            var checkPassword = await _userManager.CheckPasswordAsync(userExist, loginVM.Password);
+            if(!checkPassword)
+            {
+                _notyfService.Error("password does not match");
+                return View(loginVM);
+            }
+            await _signInManager.PasswordSignInAsync(loginVM.Username, loginVM.Password,loginVM.RememberMe, true);
+            _notyfService.Success("Login successful");
+            return RedirectToAction("Index", "User",new {area="Admin"});
         }
     }
 }
--- SalahBlog/Program.cs
+++ SalahBlog/Program.cs
@@ -1,3 +1,5 @@
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SalahBlog.Data;
@@ -20,6 +22,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 builder.Services.AddScoped<IDbInitializer,DbInitializer>();
 builder.Services.AddRazorPages()
     .AddRazorRuntimeCompilation();
+builder.Services.AddNotyf(config => { config.DurationInSeconds = 10; config.IsDismissable = true; config.Position = NotyfPosition.BottomRight; });
 
 var app = builder.Build();
 DataSeeding();
@@ -30,11 +33,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseNotyf();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
